0a6b5905e9c2c7c59ceb9b18e28583b6f1252df1
Alex Zmiev <alex.zmiev.az@gmail.com>
2020-09-30 21:37:30 +0300

    Lab 2, task 1, 4

--- Lab_2_if_for/Lab_2_if_for.cpp
+++ Lab_2_if_for/Lab_2_if_for.cpp
@@ -1,6 +1,78 @@
 ﻿#include <iostream>
 
+void task1();
+void task2();
+void task3();
+void task4();
+void chooseTask();
+
 int main()
 {
+	chooseTask();
+}
 
+void chooseTask()
+{
+	int task = 0;
+	std::cout << "choose task \n"
+		<< "1. task 1 \n"
+		<< "2. task 2 \n"
+		<< "3. task 3 \n"
+		<< "4. exit \n";
+
+	std::cin >> task;
+
+	switch (task)
+	{
+	case 1:
+		task1();
+		break;
+	case 2:
+		task2();
+		break;
+	case 3:
+		task3();
+		break;
+	case 4:
+		task4();
+		break;
+	default:
+		std::cout << "error" <<std::endl;
+		chooseTask();
+		break;
+	}
+}
+
+void task1()
+{
+	int num = 0;
+	std::cout << "enter number: ";
+	std::cin >> num;
+
+	for (int i = 0; i <= num; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			std::cout << j;
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+
+	chooseTask();
+}
+
+void task2()
+{
+
+}
+
+void task3()
+{
+
+}
+
+void task4()
+{
+	std::exit(0);
 }
