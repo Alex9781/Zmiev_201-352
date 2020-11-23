b4526077c90fd2e90a3e8a73430ebb47a5d43ded
Alex Zmiev <alex.zmiev.az@gmail.com>
2020-11-23 16:47:40 +0300

    Lab_5 comp

---
+++ Lab_5_Classes/Lab_5_Classes.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include "Matrix.h"
+
+int main()
+{
+	Matrix m1(2, 2);
+	m1.Input();
+	//m1.Print();
+	//Matrix m2(1, 2);
+	//m2.Input();
+	//m2.Print();
+
+	//Matrix m3 = Matrix::Sum(m1, m1);
+	//m3.Print();
+
+	//Matrix* m4 = Matrix::Mult(m1, m1);
+	//m4->Print();
+
+	Matrix* m4 = Matrix::Mult(m1, 10);
+	m4->Print();
+
+	//std::cout << m1.Trace() << std::endl;
+
+	//std::cout << m1.Det() << std::endl;
+
+	//Matrix m5 = Matrix::Mult(m1, 3);
+	//m5.Print();
+
+
+	/*int a = 10;
+	int* p = &a;
+	int& r = a;
+	
+	std::cout << r;*/
+
+}
---
+++ Lab_5_Classes/Matrix.h
@@ -0,0 +1,179 @@
+#pragma once
+#include <iostream>
+
+//template <typename T>
+class Matrix
+{
+private:
+	int** matr;
+	int rows;
+	int cols;
+
+	int findDet(int** a, int n)
+	{ //Рекурсивная функция вычисления определителя матрицы
+		if (n == 1)
+			return a[0][0];
+		else if (n == 2)
+			return a[0][0] * a[1][1] - a[0][1] * a[1][0];
+		else
+		{
+			int d = 0;
+			for (int k = 0; k < n; k++) {
+				int** m = new int* [n - 1];
+				for (int i = 0; i < n - 1; i++)
+				{
+					m[i] = new int[n - 1];
+				}
+				for (int i = 1; i < n; i++)
+				{
+					int t = 0;
+					for (int j = 0; j < n; j++)
+					{
+						if (j == k)
+							continue;
+						m[i - 1][t] = a[i][j];
+						t++;
+					}
+				}
+				d += pow(-1, k + 2) * a[0][k] * findDet(m, n - 1);
+			}
+			return d;
+		}
+	}
+
+public:
+	Matrix(int rows, int cols)
+	{
+		this->rows = rows;
+		this->cols = cols;
+
+		matr = new int* [rows];
+		for (int i = 0; i < rows; i++)
+		{
+			matr[i] = new int[cols];
+		}
+	}
+
+	~Matrix()
+	{
+		for (int i = 0; i < this->rows; i++)
+		{
+			delete[] this->matr[i];
+		}
+		delete[] this->matr;
+	}
+
+	void Input()
+	{
+		for (int i = 0; i < this->rows; i++)
+		{
+			for (int j = 0; j < this->cols; j++)
+			{
+				std::cin >> this->matr[i][j];
+			}
+		}
+	}
+
+	void Print()
+	{
+		for (int i = 0; i < this->rows; i++)
+		{
+			for (int j = 0; j < this->cols; j++)
+			{
+				std::cout << matr[i][j] << ' ';
+			}
+			std::cout << std::endl;
+		}
+	}
+
+	static Matrix* Sum(Matrix& m1, Matrix& m2)
+	{
+		if (m1.rows == m2.rows && m1.cols == m2.cols)
+		{
+			Matrix* res = new Matrix(m1.rows, m1.cols);
+
+			for (int i = 0; i < res->rows; i++)
+			{
+				for (int j = 0; j < res->cols; j++)
+				{
+					res->matr[i][j] = m1.matr[i][j] + m2.matr[i][j];
+				}
+			}
+
+			return res;
+		}
+	}
+
+	static Matrix* Mult(Matrix& m1, Matrix& m2)
+	{
+		if (m1.cols == m2.rows)
+		{
+			Matrix* res = new Matrix(m1.rows, m2.cols);
+			for (int i = 0; i < res->rows; i++)
+			{
+				for (int j = 0; j < res->cols; j++)
+				{
+					res->matr[i][j] = 0;
+					for (int k = 0; k < m1.cols; k++)
+					{
+						res->matr[i][j] += m1.matr[i][k] * m2.matr[k][j];
+					}
+				}
+			}
+
+			return res;
+		}
+	}
+
+	static Matrix* Mult(const Matrix& m, double x)
+	{
+		Matrix* res = new Matrix(m.rows, m.cols);
+		for (int i = 0; i < res->rows; i++)
+		{
+			for (int j = 0; j < res->cols; j++)
+			{
+				res->matr[i][j] = m.matr[i][j] * x;
+			}
+		}
+
+		return res;
+	}
+
+	double Trace()
+	{
+		if (this->rows == this->cols)
+		{
+			double res = 0;
+			for (int i = 0; i < this->rows; i++)
+			{
+				res += this->matr[i][i];
+			}
+
+			return res;
+		}
+	}
+
+	double Det()
+	{
+		if (this->rows == this->cols)
+		{
+			return this->findDet(this->matr, this->cols);
+		}
+	}
+
+	int GetRows()
+	{
+		return this->rows;
+	}
+
+	int GetCols()
+	{
+		return this->cols;
+	}
+
+	double GetElement(int i, int j)
+	{
+		return this->matr[i][j];
+	}
+};
+
