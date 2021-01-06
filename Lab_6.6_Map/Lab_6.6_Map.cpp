d7e00f9d83aabc98ca1611a037eb7655047b6734
Alex Zmiev <alex.zmiev.az@gmail.com>
2021-01-06 23:34:21 +0300

    lab 6.6 comp

---
+++ Lab_6.6_Map/Lab_6.6_Map.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+    map<string, int> m1 = map<string, int>();
+    
+    string n;
+    while (cin >> n) //ctrl+z
+    {
+        m1[n]++;
+    }
+
+    int sum = 0;
+    for (auto i : m1)
+    {
+        sum += i.second;
+    }
+
+    for (auto i : m1)
+    {
+        cout << i.first << ": " << (float)i.second / sum * 100 << "%" << endl;
+    }
+}
